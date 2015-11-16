bdb618af0d7aa6f812718f04086706472734d37a
Zach Bresnick <zbresnick@gmail.com>
2015-11-15 22:19:50 -0800

    walking skeleton

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+  stylus = require('stylus'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
+app.use(express.static(__dirname + '/public'));
+
+if(env === 'development') {
+  mongoose.connect('mongodb://localhost/first-mean-app');
+} else {
+  mongoose.connect('mongodb://localhost/first-mean-app');
+
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('first-mean-app database opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var message = mongoose.model('message', messageSchema);
+var mongoMessage;
+message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+  res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
+});
+
+var port = process.env.PORT ||  3030;
+app.listen(port);
+console.log('listening on port ' + port + '...')
